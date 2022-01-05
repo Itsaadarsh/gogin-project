cb9919212073ccc410e00e78333637a9869b4477
itsaadarsh <aadarsh.s@trell.in>
2022-01-05 15:44:27 +0530

    Adding server side validation

--- dto/user_dto.go
+++ dto/user_dto.go
@@ -2,13 +2,13 @@ package dto
 
 type UserUpdateDTO struct {
 	ID       uint64 `json:"id" form:"id" binding:"required"`
-	Name     string `json:"name" form:"name" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
+	Name     string `json:"name" form:"name" binding:"required,min=1"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:6"`
 }
 
 type UserCreatedDTO struct {
-	Name     string `json:"name" form:"name" binding:"required"`
-	Email    string `json:"email" form:"email" binding:"required" validate:"email"`
+	Name     string `json:"name" form:"name" binding:"required,min=1"`
+	Email    string `json:"email" form:"email" binding:"required,email"`
 	Password string `json:"password,omitempty" form:"password,omitempty" validate:"min:6" binding:"required"`
 }
