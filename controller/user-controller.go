56b528268d51a6bb56a97b3e97ecc539fd9b2c6d
itsaadarsh <aadarsh.s@trell.in>
2022-01-05 17:18:32 +0530

    User routes completed v1

---
+++ controller/user-controller.go
@@ -0,0 +1,71 @@
+package controller
+
+import (
+	"fmt"
+	"gogin/dto"
+	"gogin/helper"
+	"gogin/service"
+	"net/http"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
+)
+
+type UserController interface {
+	Update(ctx *gin.Context)
+	Profile(ctx *gin.Context)
+}
+
+type userController struct {
+	userSer service.UserService
+	jwtSer  service.JWTService
+}
+
+func NewUserController(userSer service.UserService, jwtSer service.JWTService) UserController {
+	return &userController{
+		userSer: userSer,
+		jwtSer:  jwtSer,
+	}
+}
+
+func (c *userController) Update(ctx *gin.Context) {
+	var userUpdateDTO dto.UserUpdateDTO
+	errDTO := ctx.ShouldBind(&userUpdateDTO)
+	if errDTO != nil {
+		response := helper.BuildErrorResponse("Failed to process request", errDTO.Error(), helper.EmptyObj{})
+		ctx.AbortWithStatusJSON(http.StatusBadRequest, response)
+		return
+	}
+
+	authHeader := ctx.GetHeader("Authorization")
+	token, errToken := c.jwtSer.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	id, err := strconv.ParseUint(fmt.Sprintf("%v", claims["user_id"]), 10, 64)
+
+	if err != nil {
+		panic(errToken.Error())
+	}
+
+	userUpdateDTO.ID = id
+	updatedUser := c.userSer.Update(userUpdateDTO)
+	response := helper.BuildResponse(true, "OK", updatedUser)
+	ctx.JSON(http.StatusOK, response)
+}
+
+func (c *userController) Profile(ctx *gin.Context) {
+	authHeader := ctx.GetHeader("Authorization")
+	token, errToken := c.jwtSer.ValidateToken(authHeader)
+	if errToken != nil {
+		panic(errToken.Error())
+	}
+
+	claims := token.Claims.(jwt.MapClaims)
+	user := c.userSer.Profile(fmt.Sprintf("%v", claims["user_id"]))
+	response := helper.BuildResponse(true, "OK", user)
+	ctx.JSON(http.StatusOK, response)
+}
--- server.go
+++ server.go
@@ -15,7 +15,9 @@ var (
 	userRep        repository.UserRepository = repository.NewUserRepository(db)
 	authSevice     service.AuthService       = service.NewAuthService(userRep)
 	jwtService     service.JWTService        = service.NewJWTService()
+	userService    service.UserService       = service.NewUserService(userRep)
 	authController controller.AuthController = controller.NewAuthController(authSevice, jwtService)
+	userController controller.UserController = controller.NewUserController(userService, jwtService)
 )
 
 func main() {
@@ -26,6 +28,12 @@ func main() {
 	{
 		authRoutes.POST("/login", authController.Login)
 		authRoutes.POST("/register", authController.Register)
+	}
+
+	userRoute := r.Group("api/user")
+	{
+		userRoute.POST("/update", userController.Update)
+		userRoute.GET("/profile", userController.Profile)
 	}
 
 	r.Run()
